b747c1009852033c4b40b8121b8124f8ceb67ea5
scarlettfres <scarlett_fres@hotmail.com>
2015-06-02 18:38:06 +0200

    quasi OK API
    TODO:
    - mini function simplification
    - nettoyage codes + doc
    - pb rvi
    - glitch

--- scripts/server_tools_mark_tracker.py
+++ scripts/server_tools_mark_tracker.py
@@ -17,8 +17,12 @@ CAMERA_NAME = "axis_camera"
 ROBOT_FOOT = "base_footprint"
 MAP = "/map"
 FILE_SAVED_MARK = "saved_mark.txt"
+FILE_SAVED_INIT_PLAN = "saved_init_plan.txt"
+FILE_SAVED_MARK_TO_ROBOT = "saved_mark_to_robot.txt"
+TIMER = 0.2
 # -- variables magiques
-chdir("/home/sfress/catkin_ws/src/mark_tracker/launch/")  # to put
+
+chdir("/home/sfress/catkin_ws/src/mark_tracker_tools/")
 
 
 def sign(var):
@@ -43,12 +47,14 @@ class ToolsPepper:
         # self.motionProxy = ALProxy("ALMotion", IP, PORT)
        # self.postureProxy = ALProxy("ALRobotPosture", IP, PORT)
         self.mark_to_save = []
-
+        self.link_to_robot = False
         self.listener = tf.TransformListener()
         self.broadcaster = tf.TransformBroadcaster()
+
         # rospy.Subscriber("/result_position", Odometry,self.position_callback)
         # rospy.Timer(rospy.Duration(TIMER_ALMOTION), self.timer_callback)
-        self.vect_tf = [[CAMERA_NAME, MAP, [0, 0, 0], [0, 0, 0, 1]]]
+        self.vect_tf = [[CAMERA_NAME, MAP, [0, 0, 0], [0, 0, 0, 1]], [
+            "ini", "child", [0, 0, 0], [0, 0, 0, 1]]]
         rospy.Subscriber(
             "/cam0/visualization_marker", Marker, self.publish_tf)
         # self.vect_tf = [CAMERA_NAME, [0, 0, 0], [0, 0, 0, 1]]
@@ -70,25 +76,56 @@ class ToolsPepper:
         rospy.Service(
             'init_mark_to_robot', InitMarkToRobot, self.init_mark_to_robot)
 
+        rospy.Service(
+            'load_mark', LoadMark, self.load_mark)
+
+        rospy.Service(
+            'load_init', LoadInit, self.load_init)
+
         self.len = 0
         # rospy.Service(
         #   'how_to_GoToMark', HowToGoToMark, self.how_to_GoToMark)
 
     # mais doit marcer ac launch init et relaunch le track no pepper
+
     def publish_tf(self, data):
         """
         publish tf contained in self.vect_tf
         """
+        # print self.vect_tf
+
         for i in range(len(self.vect_tf)):
             self.broadcaster.sendTransform(
                 self.vect_tf[i][2], self.vect_tf[i][3], rospy.Time.now(),
                 self.vect_tf[i][0], self.vect_tf[i][1])
-        if self.len != len(self.vect_tf):  # a chaque ajout
+
+        if self.len != len(self.vect_tf):  # PRINT a chaque ajout
             print"************"
             for k in range(len(self.vect_tf)):
                 print [self.vect_tf[k][0], self.vect_tf[k][1]]
             print"************"
             self.len = len(self.vect_tf)
+
+        if self.link_to_robot == True:
+            try:
+                # robot_part = str(self.vect_tf[1][0]).split("/")
+                name = str(self.vect_tf[1][0]).split("/")
+                robot_part = name[1]
+                (trans, rot) = self.listener.lookupTransform(
+                    robot_part, "base_link", rospy.Time(0))
+                # ou devrait donc etre notre base_link par rapport a notre mark: on
+                # cree un base_link "virtuel"
+                self.broadcaster.sendTransform(
+                    trans, rot, rospy.Time.now(),
+                    "/mon_tf/base_link", "/mon_tf/" + robot_part)
+                # comment est notre base_link virtuel apr rapport a notre map?
+                (trans_fin, rot_fin) = self.listener.lookupTransform(
+                    "mon_tf/base_link", MAP, rospy.Time(0))
+                self.broadcaster.sendTransform(
+                    trans_fin, rot_fin, rospy.Time.now(), MAP, "/base_link")
+
+            except Exception, exc:
+                print " waiting for tf..."
 
     def init_plan(self, req):
         """
@@ -102,21 +139,44 @@ class ToolsPepper:
                 marker, '/map', rospy.Time(0))
             self.vect_tf[0] = [CAMERA_NAME, MAP, trans, rot]
             print " plan intialized !"
+            if req.permanent == True:
+                mon_fichier = open(FILE_SAVED_INIT_PLAN, "w")
+                message = write_message([self.vect_tf[0]])
+                mon_fichier.write(message)
+                mon_fichier.close()
             return InitPlanResponse(True)
         except Exception, exc:
             print exc
 
     def init_mark_to_robot(self, req):
         """
-           link the mark to a part of the robot, then every part's coordinates
+           link the mark to a part of the robot, then every frames'coordinates
            would be accessible in our plan
         """
-
         past1sec = rospy.Time.now() - rospy.Duration(1.0)
         timeout = rospy.Duration(4.0)
+        ret = False
+        while ret != True:
+            ret = self.init_mark_to_robot_publish_temporary(req)
+        self.link_to_robot = True
+
+        if req.permanent == True:
+            mon_fichier = open(FILE_SAVED_MARK_TO_ROBOT, "w")
+            message = write_message([self.vect_tf[1]])
+            mon_fichier.write(message)
+            mon_fichier.close()
+        return InitMarkToRobotResponse(True)
+
+    def init_mark_to_robot_publish_temporary(self, req):
+        """
+            Just called by InitMarkToRobot:
+            we need to publish the tf several times, make sure that you're
+            repeatedly publishing the positions of the frames so that tf can
+            correctly interpolate. If you only publish single datapoints for
+            each frame_id at different times a lookup will never succeed
+        """
         try:
             marker = '/ar_marker_' + str(req.marknumber)
-            print "111"
             (trans_foot_to_body,
              rot_foot_to_body) = self.listener.lookupTransform(
                 ROBOT_FOOT, req.robotpart, rospy.Time(0))
@@ -125,85 +185,28 @@ class ToolsPepper:
             # jusqua present on ne sait pas ou se situe le robot dans notre
             # referentiel, pour cela on publie des donnees de la ou devrait
             # etre notre robot dans le referentiel de la map
-
             # les pieds du robot doivent etre ici par rapport a la map
             quaternion = quaternion_from_euler(0, 0, req.theta)
-            print "222"
             self.broadcaster.sendTransform(
                 (req.x, req.y, 0), quaternion, rospy.Time.now(),
-                "/mon_tf/" + ROBOT_FOOT, MAP)
-            print "333"
-
+                "mon_tf/" + ROBOT_FOOT, MAP)
             self.listener.waitForTransform(
-                "/mon_tf/" + ROBOT_FOOT, MAP, rospy.Time(), rospy.Duration(4.0))
-
+                "mon_tf/" + ROBOT_FOOT, MAP, rospy.Time(0), rospy.Duration(1.0))
             # donc notre bodypart serait ici par rapport a la map
             self.broadcaster.sendTransform(
                 trans_foot_to_body, rot_foot_to_body, rospy.Time.now(),
-                "/mon_tf/" + req.robotpart,
-                "/mon_tf/" + ROBOT_FOOT)
-            print "444"
-            # on a donc maintenant notre body_part et notre mark qui
-            # appartiennent au meme arbre : on peut connaitre la
-            # transformation qui les separe
+                "mon_tf/" + req.robotpart, "mon_tf/" + ROBOT_FOOT)
             self.listener.waitForTransform(
-                marker, "/mon_tf/" + req.robotpart, rospy.Time(), rospy.Duration(4.0))
-            # self.vect_tf.append(
-            #     ["/mon_tf/" + req.robotpart,  "/mon_tf/" + ROBOT_FOOT,
-            #      trans_foot_to_body, rot_foot_to_body])
-
-            print "555"
+                marker, "mon_tf/" + req.robotpart,
+                rospy.Time(0), rospy.Duration(1.0))
             (trans_marker_to_body,
                 rot_marker_to_body) = self.listener.lookupTransform(
-                "/mon_tf/" + req.robotpart, "/mon_tf/" + ROBOT_FOOT,
-                rospy.Time())
-
-            print "666"
-            self.vect_tf.append(
-                [marker, req.robotpart, trans_marker_to_body,
-                 rot_marker_to_body])
-            return InitMarkToRobotResponse(True)
+                marker, "mon_tf/" + req.robotpart, rospy.Time(0))
+            self.vect_tf[1] = ["mon_tf/" + req.robotpart, marker,
+                               trans_marker_to_body, rot_marker_to_body]
+            return True
         except Exception, exc:
-            print exc
-        # except (tf.LookupException, tf.ConnectivityException,
-        #         tf.ExtrapolationException):
-        #     print " waiting for tf..."
-        # self.init_mark_to_robot(req)
-
-    def write_file(self, req):
-        """
-            write in a permanent way the tf that has been published
-        """
-        try:
-           # chdir("/home/sfress/catkin_ws/src/mark_tracker/launch/")
-
-            mon_fichier = open("launch_tf_cam_map.launch", "w")
-            marker = '/ar_marker_' + str(req.marknumber)
-            trans, rot = self.listener.lookupTransform(
-                marker, '/map', rospy.Time.now())
-            message = str("""
-      <launch>
-      <node pkg="tf" type="static_transform_publisher"
-        name=""")
-            message = message + '"' + req.cameraname + \
-                '"' + str(""" args=" """)
-            message = message + str(trans[0]) + " " + str(
-                trans[1]) + " " + str(trans[2]) + " "
-            message = message + str(rot[0]) + " " + str(rot[1]) + " " + str(
-                rot[2]) + " " + str(rot[3]) + " /map /" + req.cameraname + str(
-                """ 30"/>""")
-            message = message + "</launch>"
-            print "======message saved in launchfile : "
-            print message
-            print "======"
-            mon_fichier.write(message)
-            mon_fichier.close()
-            # subprocess.call(
-            #["roslaunch /home/sfress/catkin_ws/src/mark_tracker/
-            # launch/launch_tf_cam_map.launch"])
-            return InitPlanResponse(True)
-        except Exception, exc:
-            print exc
+            print " waiting for tf..."
 
     def where_is(self, req):
         """
@@ -267,14 +270,18 @@ class ToolsPepper:
                     new = False
 
             if req.permanent == True:
-                self.save(my_mark_publish, MAP, trans, rot)
+                self.save_mark(my_mark_publish, MAP, trans, rot)
 
             return AddMarkResponse(new)
         except Exception, e:
             print"add_mark_temporary error"
             print e
 
-    def save(self, my_mark_publish, MAP, trans, rot):
+    def save_mark(self, my_mark_publish, MAP, trans, rot):
+        """
+        called by add mark if permanent==True. It checks if we already tried
+        to save our mark before to write it in the file  then it overwrite it)
+        """
         if len(self.mark_to_save) > 0:
             for i in range(0, len(self.mark_to_save)):  # if not already
                 if self.mark_to_save[i][0] != my_mark_publish:
@@ -286,33 +293,45 @@ class ToolsPepper:
             self.mark_to_save.append(
                 [my_mark_publish, MAP, trans, rot])
         mon_fichier = open(FILE_SAVED_MARK, "w")
-        message = ""
-        for k in range(0, len(self.mark_to_save)):
-            print "iiiii"
-            message = message + str(self.mark_to_save[k][0]) + "&" + str(
-                self.mark_to_save[k][1]) + "&" + str(
-                self.mark_to_save[k][2]) + "&" + str(
-                self.mark_to_save[k][3])
-
-            message = message + "\n"
-
-        print message
+        message = write_message(self.mark_to_save)
         mon_fichier.write(message)
         mon_fichier.close()
 
-    def load_mark(self):
+    def load_mark(self, req):
+        chdir(req.path)  # to put
+        temp_vect = []
+        temp_vect.append(self.vect_tf[0])
+        temp_vect.append(self.vect_tf[1])
+        fichier = numpy.genfromtxt(FILE_SAVED_MARK, delimiter=' ', dtype=None)
+        if fichier.ndim == 0:
+            fichier = fichier.ravel()
+        for k in range(0, len(fichier)):
+            toadd = [fichier[k][0], fichier[k][1],
+                     [fichier[k][i] for i in range(2, 5)],
+                     [fichier[k][i] for i in range(5, 9)]]
+            temp_vect.append(toadd)
+        print temp_vect
+        self.vect_tf = temp_vect
+        return LoadMarkResponse(True)
 
-        fichier = numpy.genfromtxt(FILE_SAVED_MARK, skiprows=1, delimiter='&')
-        frame_base = fichier[:, 0]
-        frame_child = fichier[:, 1]
-        trans = fichier[:, 2]
-        ros = fichier[:, 3]
-        # delete the current vector broadcasted
-        size = len(self.vect_tf)
-        for i in range(0, size - 2):
-            self.vect_tf.pop()
-            i = i + 1
-        self.vect_tf.append()
+    def load_init(self, req):
+        if req.init == 0:
+            indice = 0
+            doc = FILE_SAVED_INIT_PLAN
+        if req.init == 1:
+            indice = 1
+            doc = FILE_SAVED_MARK_TO_ROBOT
+            self.link_to_robot = True
+
+        chdir(req.path)
+        fichier = numpy.genfromtxt(
+            doc, delimiter=' ', dtype=None)
+        fichier = fichier.ravel()
+        toadd = [fichier[0][0], fichier[0][1],
+                 [fichier[0][i] for i in range(2, 5)],
+                 [fichier[0][i] for i in range(5, 9)]]
+        self.vect_tf[indice] = toadd
+        return LoadInitResponse(True)
 
     def how_to_go_to_mark(self, req):
         """
@@ -323,13 +342,34 @@ class ToolsPepper:
             my_mark_publish = '/my_mark_publish' + str(req.marknumber)
             (trans, rot) = self.listener.lookupTransform("/base_footprint",
                                                          my_mark_publish,
-                                                         rospy.Time.now())
+                                                         rospy.Time(0))
             euler = euler_from_quaternion(rot)
             return HowToGoToMarkResponse(trans[0], trans[1], euler[2])
 
         except Exception, e:
             print "howtgotomark() error, is ", my_mark_publish, " saved?"
             print e
+
+
+def write_message(vecteur):
+    """
+    To give the info into the form to write in the file
+    """
+    message = ""
+    for k in range(0, len(vecteur)):
+        print "iiiii"
+        print "leee", len(vecteur)
+        message = message + str(vecteur[k][0]) + " " + str(
+            vecteur[k][1]) + " " + str(
+            vecteur[k][2][0]) + " " + str(
+            vecteur[k][2][1]) + " " + str(
+            vecteur[k][2][2]) + " " + str(
+            vecteur[k][3][0]) + " " + str(
+            vecteur[k][3][1]) + " " + str(
+            vecteur[k][3][2]) + " " + str(
+            vecteur[k][3][3])
+        message = message + "\n"
+    return message
 
 
 def main(args):
@@ -350,3 +390,40 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
+
+    # maybe usefull later
+    # def write_file(self, req):
+    #     """
+    #         write in a permanent launchfile the tf that has been published
+    #     """
+    #     try:
+    # chdir("/home/sfress/catkin_ws/src/mark_tracker/launch/")
+
+    #         mon_fichier = open("launch_tf_cam_map.launch", "w")
+    #         marker = '/ar_marker_' + str(req.marknumber)
+    #         trans, rot = self.listener.lookupTransform(
+    #             marker, '/map', rospy.Time.now())
+    #         message = str("""
+    #   <launch>
+    #   <node pkg="tf" type="static_transform_publisher"
+    #     name=""")
+    #         message = message + '"' + req.cameraname + str(
+    #             '"') + str(""" args=" """)
+    #         message = message + str(trans[0]) + " " + str(
+    #             trans[1]) + " " + str(trans[2]) + " "
+    #         message = message + str(rot[0]) + " " + str(rot[1]) + " " + str(
+    #             rot[2]) + " " + str(
+    # rot[3]) + " /map /" + req.cameraname + str(
+    #             """ 30"/>""")
+    #         message = message + "</launch>"
+    #         print "======message saved in launchfile : "
+    #         print message
+    #         print "======"
+    #         mon_fichier.write(message)
+    #         mon_fichier.close()
+    # subprocess.call(
+    # ["roslaunch /home/sfress/catkin_ws/src/mark_tracker/
+    # launch/launch_tf_cam_map.launch"])
+    #         return InitPlanResponse(True)
+    #     except Exception, exc:
+    #         print exc
--- scripts/test_client_service.py
+++ scripts/test_client_service.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python
 import sys
 import rospy
-from mark_tracker_tools.srv import AddMark, HowToGo, HowToGoToMark
-from mark_tracker_tools.srv import InitMarkToRobot, InitPlan, WhereIs
+from mark_tracker_tools.srv import *
 import time
+import naoqi
+from naoqi import ALProxy
+
 if __name__ == "__main__":
 
     try:
         # give postion robot
         print "debut"
 
-        rospy.wait_for_service('init_plan')
-        init_plan = rospy.ServiceProxy('init_plan', InitPlan)
-        resp = init_plan(1)
-        print "##Initplan", resp.result
+        # rospy.wait_for_service('load_init')
+        # init_plan = rospy.ServiceProxy('load_init', LoadInit)
+        # resp = init_plan("/home/sfress/catkin_ws/src/mark_tracker_tools/", 0)
+        # print "##Initplan", resp.result
+        # time.sleep(1)  # delay needed to broadcast of tf
+
+        # rospy.wait_for_service('load_init')
+        # init_plan = rospy.ServiceProxy('load_init', LoadInit)
+        # resp = init_plan("/home/sfress/catkin_ws/src/mark_tracker_tools/", 1)
+        # print "##Initplan", resp.result
+
+        # rospy.wait_for_service('init_plan')
+        # init_plan = rospy.ServiceProxy('init_plan', InitPlan)
+        # resp = init_plan(1, True)
+        # print "##Initplan", resp.result
+
+        # time.sleep(1)  # delay needed to broadcast of tf
+
+        # rospy.wait_for_service('load_mark')
+        # load_mark = rospy.ServiceProxy('load_mark', LoadMark)
+        # resp = load_mark("/home/sfress/catkin_ws/src/mark_tracker_tools/")
+        # print "##Initplan", resp.result
 
         # time.sleep(1)  # delay needed to broadcast of tf
 
         # rospy.wait_for_service('init_mark_to_robot')
         # init_mark_to_robot = rospy.ServiceProxy(
         #     'init_mark_to_robot', InitMarkToRobot)
-        # resp = init_mark_to_robot(2, 0.57, 0, 0, "HeadTouchFront_frame")
+        # resp = init_mark_to_robot(
+        #     2, 0.60, 0.02, 0, "HeadTouchFront_frame", True)
         # print "##Initplan", resp.result
 
         # rospy.wait_for_service('where_is')
@@ -38,22 +59,28 @@ if __name__ == "__main__":
 
         # publish tf_coord of a mark
 
-        rospy.wait_for_service('add_mark')
-        add_mark = rospy.ServiceProxy('add_mark', AddMark)
-        resp = add_mark(1, True)
-        if resp.result == True:
-            print"##new mark learnt!"
-        else:
-            print "##mark overwrited!"
+        # rospy.wait_for_service('add_mark')
+        # add_mark = rospy.ServiceProxy('add_mark', AddMark)
+        # resp = add_mark(1, True)
+        # if resp.result == True:
+        # print"##new mark learnt!"
+        # else:
+        # print "##mark overwrited!"
 
         # time.sleep(1)  # delay needed to broadcast of tf
 
-        # rospy.wait_for_service('how_to_go_to_mark')
-        # how_to_go_to_mark = rospy.ServiceProxy(
-        #     'how_to_go_to_mark', HowToGoToMark)
-        # resp = how_to_go_to_mark(1)
+        rospy.wait_for_service('how_to_go_to_mark')
+        how_to_go_to_mark = rospy.ServiceProxy(
+            'how_to_go_to_mark', HowToGoToMark)
+        resp = how_to_go_to_mark(1)
 
-        # print "##I need to :  ", resp.x, resp.y, resp.theta
+        print "##I need to :  ", resp.x, resp.y, resp.theta
+
+        IP = "10.0.206.111"
+        PORT = 9559
+        motionProxy = ALProxy("ALMotion", IP, PORT)
+        motionProxy.post.moveTo(resp.x, resp.y, resp.theta)
+        motionProxy.waitUntilMoveIsFinished()
 
         """
         rospy.wait_for_service('how_to_GoToMark')
